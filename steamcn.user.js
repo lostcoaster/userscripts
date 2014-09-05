800990e826676d5cd1f6c18bb75ee3802b8d6609
lc <fjy456@gmail.com>
2014-09-05 22:29:14 +0900

    draft of the setting panel still in debug( no email send)

--- steamcn.user.js
+++ steamcn.user.js
@@ -15,30 +15,34 @@
 var timer;
 var scan_interval = 60 * 1000; //1分钟扫描间隔
 
-//todo : run a one-time-clear, or upgrading.
+
 /**
  * the options class
  * @constructor
  */
-function Options(){
+function Options() {
+    // the keys and default values, note that the values are plain values (not JSON parsed)
     var options = {
-        email:'-',
-        last_check:0,
-        latest_post:0,
-        enabled_forums:[],
-        api_key:'TrPFSHUL5CSZGPg7I2OMZQ',
-        special_users:[],
-        is_blacklist:true,
-        version:1
+        status: 0,
+        email: '',
+        last_check: 0,
+        latest_post: 0,
+        enabled_forums: [],
+        api_key: 'TrPFSHUL5CSZGPg7I2OMZQ',
+        special_users: [],
+        is_blacklist: true,
+        version: -1
     };
-    for(var key in options){
-        if(!options.hasOwnProperty(key)){continue}
+    for (var key in options) {
+        if (!options.hasOwnProperty(key)) {
+            continue
+        }
         this[key] = {
             default_val: options[key],
-            name: 'lc.steamcn.'+key,
-            get: function(){
+            name: 'lc.steamcn.' + key,
+            get: function () {
                 var temp = localStorage.getItem(this.name);
-                if(!temp){
+                if (!temp) {
                     return this.default_val;
                 } else {
                     return JSON.parse(temp);
@@ -46,23 +50,188 @@ function Options(){
             },
             set: function (val) {
                 localStorage.setItem(this.name, JSON.stringify(val))
+            },
+            remove: function () {
+                localStorage.removeItem(this.name);
             }
         };
     }
-}
 
-function latest_id(id) {
-    if (id) {
-        localStorage.setItem('lc.latest_post_id', id);
-        return id;
-    } else {
-        return Number(localStorage.getItem('lc.latest_post_id')) || 0;
+
+    var temp;
+    //upgrade from old version
+    switch (this['version'].get()) {
+        case -1:
+            //vanilla version , no version memory
+            if ((temp = localStorage.getItem('lc.notify_email')) != null) {
+                this['email'].set(temp);
+                localStorage.removeItem('lc.notify_email')
+            }
+            if ((temp = localStorage.getItem('lc.steamcn_notify_status')) != null) {
+                this['status'].set(Number(temp));
+                localStorage.removeItem('lc.steamcn_notify_status')
+            }
+            if ((temp = localStorage.getItem('lc.latest_post_id')) != null) {
+                this['latest_post'].set(Number(temp));
+                localStorage.removeItem('lc.latest_post_id')
+            }
+            localStorage.removeItem('lc.last_check');
+            break;
     }
+    //version 1 options
+    this['version'].set(1);
 }
 
+var options = new Options();
+
+function construct_setting_panel() {
+    // setting some custom styles
+    jQuery('<style>.lc-selected-forum !important{ border: 3px solid lightblue; }</style>').appendTo('head');
+
+    // a long code , direct copypasta from the steamcn forum, yep.
+    var panel = jQuery('<div id="lc-setting-panel" class="fwinmask" style="padding: 0 5px; position: fixed; z-index: 201; left: 632.5px; top: 286px;"><table cellpadding="0" cellspacing="0" class="fwin"><tbody><tr><td class="t_l"></td><td class="t_c"></td><td class="t_r"></td></tr><tr><td class="m_l"></td><td class="m_c"><h3 class="flb">调整推送器设置<span><a href="javascript:;" class="flbc" title="关闭">关闭</a></span></h3><div id="lc-panel-container"></div></td><td class="m_r"></td></tr><tr><td class="b_l"></td><td class="b_c"></td><td class="b_r"></td></tr></tbody></table></div>');
+    panel.find('.flbc').click(function () {
+        panel.remove();
+    });
+    // filling it with the parts
+    var container = panel.find('#lc-panel-container');
+    container.css('padding', '5px');
+    var enable_container = jQuery('<div>推送功能: <label><input type="radio" name="lc-enable-switch" value="1">启用</label><label><input type="radio" name="lc-enable-switch" value="0">关闭</label></div>');
+    enable_container.find('input[value="' + options.status.get() + '"]').prop('checked', 'true');
+//    enable_container.find('input').change(function(e){
+//        if(!e.target.checked) {return}
+//        set_enable(e.target.value == '1');
+//    });
+    container.append(enable_container);
+    var forum_list_container = jQuery('<div id="lc-forum-list"><ul class="pbl cl"><li count="0"></li><li count="1"></li><li count="2"></li></ul></div>');
+    container.append(forum_list_container);
+    var special_name_container = jQuery('<div><span>黑/白名单的用户名<br>一行一个</span><textarea style="float: left;"></textarea><label><input type="radio" name="lc-black-switch" value="1">黑名单</label><br><label><input type="radio" name="lc-black-switch" value="0">白名单</label></div>');
+    special_name_container.find('input[value="' + (options.is_blacklist.get() ? 1 : 0) + '"]').prop('checked', 'true');
+    special_name_container.find('textarea').val(options.special_users.get().join('\n'));
+    container.append(special_name_container);
+    var email_container = jQuery('<p><label>邮箱 (如果不需要邮箱推送, 就留空): <input></label></p>');
+    email_container.find('input').val(options.email.get());
+    container.append(email_container);
+    var api_key_container = jQuery('<div><label>API Key (如果不知道那是什么, 就不要修改): <input></label></div>');
+    api_key_container.find('input').val(options.api_key.get());
+    container.append(api_key_container);
+    var save_btn = jQuery('<div><button class="pn pnc y"><span>保存并应用更改</span></button></div>');
+    save_btn.click(save_data);
+    container.append(save_btn);
+
+    var forum_data = jQuery('<div/>');
+    var selected_forums = options.enabled_forums.get();
+    /**
+     * parse the retrieved data and store them in enclosure
+     * @param data
+     */
+    function set_forum_data(data) {
+        forum_data.html(data.childNodes[0].childNodes[0].textContent);
+
+        forum_data.find('#fs_group').prepend('<li fid="common">常用版块</li>');
+        //add class
+        forum_data.find('div:eq(0) li').each(function (i, d) {
+            if (forum_data.find('ul[id$="forum_' + d.getAttribute('fid') + '"]').length != 0) {
+                d.className = 'pbsb';
+            }
+        });
+
+        switch_forum(0);
+    }
+
+    /**
+     * a callback handling forum name clicking
+     * @param {number}dest_col
+     * @param {string=}fid
+     */
+    function switch_forum(dest_col, fid) {
+        var forum_list_container = jQuery('#lc-forum-list');
+        var dataset;
+        switch (dest_col) {
+            case 0:
+                dataset = forum_data.find('#fs_group>li');
+                break;
+            case 1:
+                dataset = forum_data.find('#fs_forum_' + fid + '>li');
+                break;
+            case 2:
+                dataset = forum_data.find('#fs_subforum_' + fid + '>li');
+                break;
+            default:
+                return;
+        }
+        forum_list_container.find('li:gt(' + (dest_col - 1) + ')').empty();
+        var target = forum_list_container.find('li').eq(dest_col);
+        jQuery.map(dataset, function (d) {
+            var inserting_holder = jQuery('<p><a href="javascript:;"></a></p>');
+            var inserting = inserting_holder.find('a');
+            if(d.className){
+                inserting.addClass(d.className);
+            }
+            if(selected_forums.indexOf(d.getAttribute('fid')) > -1){
+                // in the previous setting
+                inserting.addClass('lc-seleced-forum');
+            }
+            inserting.attr('fid', d.getAttribute('fid'));
+            inserting.text(d.innerText);
+            target.append(inserting_holder);
+        });
+        // the clicking handler
+        target.find('a').click(function (e) {
+            var elem_fid = e.target.getAttribute('fid');
+
+            if (e.target.parentNode.className && elem_fid != 'common') { // remember the non forum collection
+                // if already select, toggle it in the list
+                var index = selected_forums.indexOf(elem_fid);
+                if(index != -1){
+                    jQuery(e.target).removeClass('lc-selected-forum');
+                    selected_forums.splice(index,1); // delete
+                } else {
+                    jQuery(e.target).addClass('lc-selected-forum');
+                    selected_forums.push(elem_fid); // add
+                }
+            } else {
+                // otherwise just select
+                jQuery('.pbls').removeClass('pbls');
+                e.target.parentNode.className = 'pbls';
+            }
+
+            switch_forum(Number(e.target.parentNode.parentNode.getAttribute('count')) + 1, elem_fid);
+        });
+    }
+
+    /**
+     * the callback handling save configuration data
+     */
+    function save_data() {
+        options.status.set(Number(enable_container.find('input:checked').val()));
+        options.enabled_forums.set(selected_forums);
+        var temp = special_name_container.find('textarea').text().split('\n');
+        temp = jQuery.map(temp, function(d){return jQuery.trim(d)}).filter(function(d){return d});
+        options.special_users.set(temp);
+        options.is_blacklist.set(special_name_container.find('input:checked').val() == '1');
+        temp = jQuery.trim(email_container.find('input').text());
+        temp = temp ? temp:'-';
+        options.email.set(temp);
+        options.api_key.set(api_key_container.find('input').text());
+        location.reload(); // refresh
+    }
+
+
+    jQuery.get('http://steamcn.com/forum.php?mod=misc&action=nav&infloat=yes&handlekey=nav&inajax=1&ajaxtarget=fwin_content_nav')
+        .done(set_forum_data);
+
+    jQuery('body').append(panel);
+}
+
+/**
+ * send emails
+ * @param {object[]}contents
+ */
 function send_email(contents) {
-    var email = localStorage.getItem('lc.notify_email');
-    if (!email) {
+    return; //todo
+    var email = options.email.get();
+    if (!email || email == '-') {
         return; // not to send
     }
 
@@ -72,10 +241,10 @@ function send_email(contents) {
         }).join('<br><br>');
     var text_content = '以下是推送内容: \n\n' +
         jQuery.map(contents, function (data) {
-            return data.title+'\n'+data.detail+'\n'+data.link;
+            return data.title + '\n' + data.detail + '\n' + data.link;
         }).join('\n');
     var req_data = {
-        "key": "TrPFSHUL5CSZGPg7I2OMZQ", // APIkey, 如果额度满了, 可能会有改动(估计满不了)
+        "key": options.api_key.get(),
         "message": {
             "html": html_content,
             "text": text_content,
@@ -109,8 +278,12 @@ function send_email(contents) {
         url: 'https://mandrillapp.com/api/1.0/messages/send.json',
         data: req_data
     })
-        .done(function(d){console.debug('Email sent, response -- ' + d)})
-        .fail(function(){console.error('Email sending failed')})
+        .done(function (d) {
+            console.debug('Email sent, response -- ' + d)
+        })
+        .fail(function () {
+            console.error('Email sending failed')
+        })
 }
 
 function check_for_post() {
@@ -118,11 +291,11 @@ function check_for_post() {
 
     //avoid multiple tabs check simultaneously, resulting some confusing results.
     var now = (new Date()).getTime();
-    if (now - Number(localStorage.getItem('lc.last_check') || '0') <= scan_interval / 2) {
+    if (now - options.last_check.get() <= scan_interval / 2) {
         // there will be one stopping
         return;
     } else {
-        localStorage.setItem('lc.last_check', now.toString());
+        options.last_check.set(now);
     }
 
     jQuery.get('http://steamcn.com/forum.php?mod=guide&view=newthread').done(function (data) {
@@ -133,7 +306,7 @@ function check_for_post() {
             var entry = jQuery(d);
             var entry_id = Number(entry.attr('id').match(/\d+/)[0]);
             var entry_link = entry.find('.common a').prop('href');
-            if (entry_id > latest_id()) {
+            if (entry_id > options.latest_post.get()) {
                 max_id = Math.max(max_id, entry_id); // set max
                 var note = new Notification(entry.find('.common a').text(), {
                     body: '由' +
@@ -162,51 +335,33 @@ function check_for_post() {
             }
         });
 
-        if(found_post.length > 0){
+        if (found_post.length > 0) {
             send_email(found_post);
-            latest_id(max_id);
+            options.latest_post.set(max_id);
         }
     });
 }
 
 function set_enable(enabled) {
-    var insert_elem = jQuery('.lc-pushing-switch');
     if (enabled) {
-        localStorage.setItem('lc.steamcn_notify_status', '1');
-        insert_elem.find('a').text('关闭推送');
-        insert_elem.click(function () {
-            set_enable(false);
-        });
+        options.status.set(1);
 
-        if (!localStorage.getItem('lc.notify_email')) {
-            var email = jQuery.trim(window.prompt('可以开启邮箱推送功能(测试). 需要开启吗? \n 需要请输入邮箱 \n 不需要直接确定即可'));
-            if (email) {
-                alert('你输入的邮箱是' + email + ', 如果有误, 请点击关闭推送, 然后重新打开输入.');
-                localStorage.setItem('lc.notify_email', email);
-            } else {
-                localStorage.setItem('lc.notify_email', '');
-            }
-        }
         timer = setInterval(check_for_post, scan_interval);
     } else {
-        localStorage.setItem('lc.steamcn_notify_status', '0');
-        insert_elem.find('a').text('启动推送');
-        insert_elem.click(function () {
-            set_enable(true);
-        });
-        localStorage.removeItem('lc.notify_email');
+        options.status.set(0);
         clearInterval(timer);
     }
 }
 
 function init() {
 
-    var insert_elem = jQuery('<li class="lc-pushing-switch"><a></a></li>');
+    var insert_elem = jQuery('<li class="lc-pushing-switch"><a>设置推送</a></li>');
     insert_elem.css('cursor', 'pointer');
     insert_elem.css('font-weight', 'bold');
+    insert_elem.click(construct_setting_panel);
     jQuery('#umnav_menu').append(insert_elem);
 
-    set_enable(localStorage.getItem('lc.steamcn_notify_status') == '1');
+    set_enable(options.status.get() != 0);
 }
 
 jQuery(init);
